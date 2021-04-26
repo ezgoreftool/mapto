7ceab11e1c178d18b995f4f811a36d7e9f002ca8
Josué Rodriguez <code@josue.io>
2021-04-26 04:41:16 -0400

    added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ezgoreftool/mapto
+
+go 1.16
---
+++ map_to.go
@@ -0,0 +1,23 @@
+package mapto
+
+import (
+	"encoding/json"
+)
+
+func MapToJSON(data map[string]interface{}) string {
+	jsonPayload, _ := json.Marshal(data)
+	return string(jsonPayload)
+}
+
+func StructToMap(data interface{}) (map[string]interface{}, error) {
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	mapData := make(map[string]interface{})
+	err = json.Unmarshal(dataBytes, &mapData)
+	if err != nil {
+		return nil, err
+	}
+	return mapData, nil
+}
